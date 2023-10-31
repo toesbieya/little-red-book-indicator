571cb20cc4484fad49506e02ea5ed4645279e63d
toesbieya <1647775459@qq.com>
2023-10-31 10:48:10 +0800

    yes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/little-red-book-indicator/dist/',
+  base: '/little-red-book-indicator/',
   plugins: [
     vue(),
   ],
